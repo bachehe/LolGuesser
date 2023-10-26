f11a102c3be263b6b0b9ebdcffc21726eed33644
Damian Bachowski <bachowski.d@e80group.com>
2023-10-26 16:09:27 +0200

    update ui

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Character } from './models/character';
+import { Character } from './shared/models/character';
 
 @Component({
   selector: 'app-root',
