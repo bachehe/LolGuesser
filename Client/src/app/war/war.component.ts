bf89f079d907e091ef0f479740bf1a877b35685a
Damian Bachowski <bachowski.d@e80group.com>
2023-10-27 14:54:43 +0200

    feat winner

--- Client/src/app/war/war.component.ts
+++ Client/src/app/war/war.component.ts
@@ -10,17 +10,64 @@ import { WarService } from './war.service';
 
 export class WarComponent implements OnInit{
   champions: Character[] = [];
+  winner: any;
+  leftHigher: boolean = false;
+  rightHigher: boolean = false;
+  userWinner: string = '';
 
   constructor(private warService: WarService){}
 
   ngOnInit(): void {
-    // this.warService.getWarCharacters().subscribe({
-    //   next: response => this.champions = response,
-    // });
+    this.onButtonClick();
   }
+
   onButtonClick(): void {
     this.warService.getWarCharacters().subscribe({
       next: response => this.champions = response,
     });
+
+
+}
+  private winnerCharacter(): void {
+    this.leftHigher = false;
+    this.rightHigher = false;
+    this.winner = '';
+
+    const attributesToCompare: (keyof Character)[] = ['hp', 'ap', 'ad', 'hpGain'];
+
+    for (const attribute of attributesToCompare) {
+      if (this.champions[0][attribute] > this.champions[1][attribute]) {
+        this.leftHigher = true;
+        this.winner = this.champions[0].name;
+      } else if (this.champions[0][attribute] < this.champions[1][attribute]) {
+        this.rightHigher = true;
+        this.winner = this.champions[1].name;
+      }
+      if (this.winner) {
+        break;
+      }
+    }
+  }
+
+  userChoice(side: string): void{
+    this.winnerCharacter();
+    if(side === 'left'){
+       this.userWinner = this.champions[0].name;
+    }
+    else if(side ==='right'){
+       this.userWinner = this.champions[1].name;
+    }
+    if(this.userWinner === this.winner){
+      this.onWin();
+    }
+    else{
+      this.onLost();
+    }
+  }
+  private onWin(): void{
+    console.log('you win');
+  }
+  private onLost(): void{
+    console.log('you lost');
   }
 }
