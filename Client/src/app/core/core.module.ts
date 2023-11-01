8baa0a2aaccaffd792d34e8f2d3a764f15707c76
Damian Bachowski <bachowski.d@e80group.com>
2023-11-01 21:15:37 +0100

    update components

--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { ToastrModule } from 'ngx-toastr';
 
 
 
@@ -11,7 +12,11 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     RouterModule,
-    CommonModule
+    CommonModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })
   ],
   exports:[
     NavBarComponent,
--- Client/src/app/core/interceptors/error.interceptor.ts
+++ Client/src/app/core/interceptors/error.interceptor.ts
@@ -8,16 +8,18 @@ import {
 } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private toastr: ToastrService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
         if(error){
+          this.toastr.error(error.error.message, "Some errors occured:(")
           this.router.navigateByUrl('');
         };
         return throwError(() => new Error(error.message));
