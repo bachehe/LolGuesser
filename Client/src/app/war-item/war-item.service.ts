cbbb99c97ac00c95ced57a1f35d0a0698637c6aa
Damian Bachowski <bachowski.d@e80group.com>
2023-11-15 11:09:17 +0100

    udpate client

--- Client/src/app/war-item/war-item.service.ts
+++ Client/src/app/war-item/war-item.service.ts
@@ -11,9 +11,6 @@ export class WarItemService {
   baseUrl = 'https://localhost:7224/api/'
   constructor(private http: HttpClient) {}
 
-  getChampions(): Observable<{ item: string[], itemPictureUrl: string[], character: Character[] }> {
-    return this.http.get<{ item: string[], itemPictureUrl: string[], character: Character[] }>(this.baseUrl + 'Character/item-war');
-  }
   getWarCharacters(){
     return this.http.get<any>(this.baseUrl + 'Character/item-war')
   }
