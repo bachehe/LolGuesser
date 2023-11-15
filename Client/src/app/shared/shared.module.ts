2447aab0f8ed6ec763aaf10f899f9d3db581c668
Damian Bachowski <bachowski.d@e80group.com>
2023-11-15 13:52:20 +0100

    update reusable

--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
-import { WarItemH1Component } from './war-item-h1/war-item-h1.component';
+import { WarItemH1Component } from './war-h1/war-item-h1.component';
+import { WarLostComponent } from './war-lost/war-lost.component';
 
 
 
 @NgModule({
   declarations: [
     FooterComponent,
-    WarItemH1Component
+    WarItemH1Component,
+    WarLostComponent
   ],
   imports: [
     CommonModule,
   ],
   exports:[
     FooterComponent,
-    WarItemH1Component
+    WarItemH1Component,
+    WarLostComponent
   ]
 })
 export class SharedModule { }
