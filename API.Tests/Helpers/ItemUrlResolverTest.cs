3425c83d04cf6534d7a4479dfa593e9708b4c568
Damian Bachowski <bachowski.d@e80group.com>
2023-11-16 16:14:08 +0100

    update tests

--- API.Tests/Helpers/ItemUrlResolverTest.cs
+++ API.Tests/Helpers/ItemUrlResolverTest.cs
@@ -6,36 +6,37 @@ using Microsoft.Extensions.Configuration;
 using Moq;
 using System.ComponentModel.DataAnnotations;
 
+
 namespace API.Tests.Helpers
 {
-    public class CharacterUrlResolverTest
+    public class ItemUrlResolverTest
     {
         [Fact]
         public void Resolve_ReturnsEmptyString_EmptyUrl()
         {
             var cfgMock = new Mock<IConfiguration>();
-            var resolver = new CharacterUrlResolver(cfgMock.Object);
-            var source = new Character { PictureUrl = null };
-            var destination = new CharacterDto();
+            var resolver = new ItemUrlResolver(cfgMock.Object);
+            var source = new Item { PictureUrl = null };
+            var destination = new ItemDto();
 
             var result = resolver.Resolve(source, destination, nameof(destination.PictureUrl), null);
 
             Assert.Empty(result);
         }
         [Theory]
-        [InlineData("images/Champions/Sona.jpg")]
-        [InlineData("images/Champions/Aatrox.jpg")]
-        [InlineData("images/Champions/Akali.jpg")]
-        [InlineData("images/Champions/Swain.jpg")]
-        [InlineData("images/Champions/Kennen.jpg")]
+        [InlineData("images/Items/Eclipse.jpg")]
+        [InlineData("images/Items/BlackCleaver.jpg")]
+        [InlineData("images/Items/FrozenMallet.jpg")]
+        [InlineData("images/Items/LichBane.jpg")]
+        [InlineData("images/Items/Statik.jpg")]
         public void Resolve_ReturnsFullUrl_WhenPictureUrlIsNotEmpty(string url)
         {
             var expectedApiUrl = "https://localhost:7224/";
             var cfgMock = new Mock<IConfiguration>();
             cfgMock.Setup(c => c["ApiUrl"]).Returns(expectedApiUrl);
-            var resolver = new CharacterUrlResolver(cfgMock.Object);
-            var source = new Character { PictureUrl = url };
-            var destination = new CharacterDto();
+            var resolver = new ItemUrlResolver(cfgMock.Object);
+            var source = new Item { PictureUrl = url };
+            var destination = new ItemDto();
 
             var result = resolver.Resolve(source, destination, nameof(destination.PictureUrl), null);
 
--- API.Tests/Helpers/WarChampionsTest.cs
+++ API.Tests/Helpers/WarChampionsTest.cs
@@ -83,6 +83,103 @@ namespace API.Tests.Helpers
             Assert.Null(selector);
         }
 
+        [Fact]
+        public void CreateChampionsWithItemList_EmptyLists_ReturnExpected()
+        {
+            var champions = new List<object>();
+            var items = new List<ItemDto>();
+
+            var result = WarChampions.CreateChampionsWithItemList(champions, items);
+
+            Assert.Empty(result.Character);
+            Assert.Empty(result.Item);
+            Assert.Empty(result.ItemPictureUrl);
+        }
+
+        [Fact]
+        public void CreateChampionsWithItemList_ReturnsExpected()
+        {
+            var mockChampions = new List<object> { };
+            var mockItems = new List<ItemDto>
+            {
+                new ItemDto { Name = "Item1", PictureUrl = "url" },
+                new ItemDto { Name = "Item2", PictureUrl = "url" }
+            };
+
+            var result = WarChampions.CreateChampionsWithItemList(mockChampions, mockItems);
+
+            Assert.Equal(mockChampions, result.Character);
+            Assert.Equal(2, result.Item.Count());
+            Assert.Contains("Item1", result.Item);
+            Assert.Contains("url", result.ItemPictureUrl);
+            Assert.Contains("Item2", result.Item);
+            Assert.Contains("url", result.ItemPictureUrl);
+        }
+
+        [Theory]
+        [InlineData(50, 50, 0.1, 50, 5, 0.1, 50, 50, 0.1, 50, 50, 0.1, 50, 50, 0.01, 50, 50, 0.1, 150, 150, 0.1111, 150, 5, 0.121)]
+        [InlineData(50, 50, 1, 50, 100, 1, 50, 50, 1, 50, 50, 1,50, 50, 1, 50, 50, 1,150, 150, 0.4, 150, 200, 4)]
+        [InlineData(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0,0, 0, 0.1, 0, 0, 0)]
+        public void MergeChampionWithItems_(
+            int ad, int armor, decimal As, int hp, int mana, decimal ms,
+            int iad1, int iarmor1, decimal iAs1, int ihp1, int imana1, decimal ims1,
+            int iad2, int iarmor2, decimal iAs2, int ihp2, int imana2, decimal ims2,
+            int ead, int earmor, decimal eAs, int ehp, int emana, decimal ems)
+        {
+            var champion = new CharacterDto()
+            {
+                PictureUrl = "url",
+                Name = "name",
+                Ad = ad,
+                Armor = armor,
+                As = 0.1m,
+                ArmorGain = As,
+                Hp = hp,
+                HpGain = 50,
+                Mana = mana,
+                ManaGain = 50,
+                Mr = 50,
+                MS = ms,
+                Range = 50
+            };
+            var items = new List<ItemDto>()
+            {
+                new ItemDto()
+                {
+                    Name = "name",
+                    Ad = iad1,
+                    MS = ims1,
+                    Mr= 50,
+                    Armor = iarmor1,
+                    As = iAs1,
+                    Hp = ihp1,
+                    Mana= imana1,
+                    PictureUrl= "url",
+                },
+                new ItemDto()
+                {
+                    Name = "name",
+                    Ad = iad2,
+                    MS = ims2,
+                    Mr= 50,
+                    Armor = iarmor2,
+                    As = iAs2,
+                    Hp = ihp2,
+                    Mana= imana2,
+                    PictureUrl= "url",
+                }
+            };
+
+            WarChampions.MergeChampionWithItems(champion, items);
+
+            Assert.Equal(ead, champion.Ad);
+            Assert.Equal(earmor, champion.Armor);
+            Assert.Equal(eAs, champion.As);
+            Assert.Equal(ehp, champion.Hp);
+            Assert.Equal(emana, champion.Mana);
+            Assert.Equal(ems, champion.MS);
+        }
+
         //TODO
         [Fact]
         public void SelectObjects_EmptyList_ReturnsNotNull()
@@ -94,5 +191,5 @@ namespace API.Tests.Helpers
             Assert.NotNull(result);
         }
     }
-   
+
 }
