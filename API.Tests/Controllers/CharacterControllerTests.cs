8ec31f425941fb558299fc26eca304fe407ae308
Damian Bachowski <bachowski.d@e80group.com>
2024-01-17 14:54:37 +0100

    add tests

--- API.Tests/Controllers/CharacterControllerTests.cs
+++ API.Tests/Controllers/CharacterControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using API.DTO;
+﻿using API.Controllers;
+using API.DTO;
 using API.Services;
 using AutoMapper;
 using Core.Entities;
@@ -15,25 +16,46 @@ namespace API.Tests.Controllers
 {
     public class CharacterControllerTests
     {
+        private readonly Mock<IWarService> _warService;
         public CharacterControllerTests()
         {
-
+            _warService = new Mock<IWarService>();
         }
         [Fact]
-        public async Task GetCharacters_Returns_OkResult()
+        public async Task GetCharacters_Returns_OkResult_WithCharacters()
         {
-            // Arrange
-            var mockService = new Mock<IYourService>();
-            mockService.Setup(s => s.GetCharacters()).ReturnsAsync(new List<CharacterDto> { /* characters here */ });
-            var controller = new YourController(mockService.Object);
+            var characters = new List<CharacterDto> { new CharacterDto {} };
+            _warService.Setup(s => s.GetCharacters()).ReturnsAsync(characters);
+            var controller = new CharacterController(_warService.Object);
 
-            // Act
             var result = await controller.GetCharacters();
 
-            // Assert
-            Assert.IsType<OkObjectResult>(result.Result);
-            var okResult = (OkObjectResult)result.Result;
-            Assert.IsType<List<CharacterDto>>(okResult.Value);
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var returnedCharacters = Assert.IsType<List<CharacterDto>>(okResult.Value);
+            Assert.Equal(characters, returnedCharacters);
+        }
+
+        [Fact]
+        public async Task GetCharacters_Returns_NoContent_When_ServiceReturnsEmptyList()
+        {
+            var characters = new List<CharacterDto>();
+            _warService.Setup(s => s.GetCharacters()).ReturnsAsync(characters);
+            var controller = new CharacterController(_warService.Object);
+
+            var result = await controller.GetCharacters();
+
+            Assert.IsType<NoContentResult>(result.Result);
+        }
+
+        [Fact]
+        public async Task GetCharacters_Returns_BadRequest_When_ServiceReturnsNull()
+        {
+            _warService.Setup(s => s.GetCharacters()).ReturnsAsync((List<CharacterDto>)null);
+            var controller = new CharacterController(_warService.Object);
+
+            var result = await controller.GetCharacters();
+
+            Assert.IsType<BadRequestResult>(result.Result);
         }
     }
 }
